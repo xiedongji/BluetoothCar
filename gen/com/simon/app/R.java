77f4ee7f84cd7b9ddbc7b6a8c1a33d40523d32dd
xiedongji@jrzhuxue.com <simonxie123@qq.com>
2016-12-14 11:21:58 +0800

    layout mgr

--- gen/com/simon/app/R.java
+++ gen/com/simon/app/R.java
@@ -22,10 +22,26 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000f;
+        public static final int btnA=0x7f08000d;
+        public static final int btnB=0x7f08000e;
+        public static final int btnDown=0x7f080004;
+        public static final int btnLeft=0x7f080002;
+        public static final int btnReset=0x7f080006;
+        public static final int btnRight=0x7f080003;
+        public static final int btnSet=0x7f080007;
+        public static final int btnUp=0x7f080001;
+        public static final int btnX=0x7f08000a;
+        public static final int btnY=0x7f08000b;
+        public static final int leftPanel=0x7f080000;
+        public static final int llab=0x7f08000c;
+        public static final int llxy=0x7f080009;
+        public static final int middlePanel=0x7f080005;
+        public static final int rightPanel=0x7f080008;
     }
     public static final class layout {
         public static final int act_home=0x7f030000;
